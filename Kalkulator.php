1f839b86bf146f82270b1287b8aefafb490265e7
suprayetno <56019744+suprayetetno@users.noreply.github.com>
2019-12-24 13:55:32 +0700

    Add files via upload

---
+++ Kalkulator.php
@@ -0,0 +1,14 @@
+
+
+<form action="" method="post">
+    <input type="number" name="number1">
+    <select name="hitung">
+        <option value="tambah"> + </option>
+        <option value="kurang"> - </option>
+        <option value="bagi"> / </option>
+        <option value="kali"> x </option>
+        <option value="sisa_bagi"> % </option>
+    </select>
+    <input type="number" name="number2">
+    <button type="submit" name="submit"> Hitung</button>
+</form>
